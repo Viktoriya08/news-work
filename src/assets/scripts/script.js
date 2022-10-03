2717156bf42723306d7688ab3865111af8b8d87a
Viktoriya08 <70775279+Viktoriya08@users.noreply.github.com>
2022-10-03 18:00:48 +0300

    Add files via upload

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,25 @@
+// script.js
+
+// Script
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    window.app = {};
+
+    // Plugins
+
+    @@include('./plugins/air-datepicker.js');
+    @@include('./plugins/swiper.js');
+    @@include('./plugins/simplebar.js');
+
+    // Blocks
+
+    @@include('./blocks/open-popup.js');
+    @@include('./blocks/calendar.js');
+    @@include('./blocks/choise.js');
+    @@include('./blocks/news.js');
+    @@include('./blocks/burger.js');
+    @@include('./blocks/news-slider.js');
+    @@include('./blocks/news-backend.js');
+
+});
